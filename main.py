2ee26dbce758676ade04c226505d32f872da13e9
dlwns412 <ljuni0412@gmail.com>
2025-06-09 12:36:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,6 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# ✅ 한글 폰트 설정 (Mac/Windows/Linux 자동 감지)
-import platform
-from matplotlib import font_manager, rc
 
 if platform.system() == 'Windows':
     font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
