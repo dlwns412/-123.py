66336e7e255d88b1e0fd6b6ea9f7f90f8ad21cf8
dlwns412 <ljuni0412@gmail.com>
2025-06-16 12:28:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-st.write(year_gender_age.columns.tolist())
-st.write(year_gender_age.head())
+filtered_df = year_gender_age[year_gender_age['감염병명'].str.contains('|'.join(sti_keywords), case=False, na=False)]
+st.write(f"필터링된 행 개수: {len(filtered_df)}")
