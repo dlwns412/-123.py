0074c0cace42367d19d9edfc0eaaa5264f68e57c
dlwns412 <ljuni0412@gmail.com>
2025-06-16 12:13:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import streamlit as st
 import pandas as pd
 
-df = pd.read_csv("감염병_군별_발생현황_20250602122005.csv", encoding='cp949')
-print(df.columns)
+df = pd.read_csv("감염병_군별_발생현황_20250602122005.csv", encoding="cp949")
+st.write("✅ 데이터 샘플:")
+st.dataframe(df)
+st.write("✅ 컬럼명:")
+st.write(df.columns.tolist())
