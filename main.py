846392f92c9841246f16767c1d869bc879d48c9b
dlwns412 <ljuni0412@gmail.com>
2025-06-16 12:30:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-filtered_df = year_gender_age[year_gender_age['감염병명'].str.contains('|'.join(sti_keywords), case=False, na=False)]
-st.write(f"필터링된 행 개수: {len(filtered_df)}")
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("감염병_연도별_및_연령별__성별_발생수_20250602121929.csv", encoding="cp949")
+
+st.write("데이터 컬럼명 리스트:")
+st.write(df.columns.tolist())
+
+st.write("데이터 샘플 (상위 5개 행):")
+st.write(df.head())
